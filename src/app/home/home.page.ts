7e5bcb45ea8ff11d28b039cffc511eecd974fa06
Jake Coates <Jakecoates@programmer.net>
2021-08-05 23:48:56 +0100

    Get the map in finally, zooming numbers were causing issues

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ClassificationService } from '../classification/services/classification.service';
-import { take } from 'rxjs/operators';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import Classification from '../classification/models/classification.model';
-import copy from 'copy-to-clipboard';
 import {Map} from 'leaflet';
 import { ToastController } from '@ionic/angular';
 
@@ -20,47 +16,12 @@ export class HomePage implements OnInit {
   private zoom: number;
 
   constructor(
-    public classification: ClassificationService,
     public formBuilder: FormBuilder,
     public toastController: ToastController
   ) {
   }
 
   ngOnInit(): void {
-    this.generateFormGroup();
-  }
-
-  generateFormGroup() {
-    this.classificationForm = this.formBuilder.group({
-      text: ['', Validators.required]
-    });
-  }
-
-  classify() {
-    const text: Classification = this.classificationForm.value;
-    this.classification.classifyText(text).pipe(take(1)).subscribe((result) => {
-      this.classification.historicalClassifications.unshift(result);
-    })
-  }
-
-  copyText(submission) {
-    copy(submission.text, {
-      onCopy: async(clipboardData: object) => {
-        const toast = await this.toastController.create({
-          message: `'${submission.text}' has been copied`,
-          duration: 2000
-        });
-        toast.present();
-      }}
-    );
-  }
-
-  receiveMap(map: Map) {
-    this.map = map;
-  }
-
-  receiveZoom(zoom: number) {
-    this.zoom = zoom;
   }
 
 }
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,26 +1,34 @@
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
-import { Map, latLng, MapOptions, tileLayer, ZoomAnimEvent } from 'leaflet';
+import { AfterViewInit, Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Map, latLng, MapOptions, tileLayer, ZoomAnimEvent, TileLayer, TileLayerOptions, LatLng } from 'leaflet';
+import { BehaviorSubject } from 'rxjs';
+
+export const MAX_ZOOM = 18;
+export const MAX_NATIVE_ZOOM = 18;
+export const INITIAL_ZOOM = 17;
+
+
+export const MAP_OPTIONS = {
+  zoomControl: false,
+  maxZoom: MAX_ZOOM,
+  tap: false, // https://github.com/Leaflet/Leaflet/issues/7255
+  doubleClickZoom: false,
+  attributionControl: false
+};
+export const LONDON_COORDINATES: LatLng = new LatLng(51.5074, 0.1278);
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss'],
 })
-export class MapComponent implements OnInit, OnDestroy {
-  @Output() map$: EventEmitter<Map> = new EventEmitter();
-  @Output() zoom$: EventEmitter<number> = new EventEmitter();
-  @Input() options: MapOptions= {
-                      layers:[tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                        opacity: 0.7,
-                        maxZoom: 19,
-                        detectRetina: true,
-                        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                      })],
-                      zoom:1,
-                      center:latLng(0,0)
-  };
+export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
   public map: Map;
   public zoom: number;
+  // Subject for map to know when it initialised
+  public mapCreated$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+
+
+  private tiles: TileLayer;
 
   constructor() {
   }
@@ -32,16 +40,76 @@ export class MapComponent implements OnInit, OnDestroy {
     this.map.clearAllEventListeners();
     this.map.remove();
   };
-
-  onMapReady(map: Map) {
-    this.map = map;
-    this.map$.emit(map);
-    this.zoom = map.getZoom();
-    this.zoom$.emit(this.zoom);
+  ngAfterViewInit(): void {
+    // an optimal time for view to enter and map to load properly
+    setTimeout(() => this.initMap(), 500);
   }
 
-  onMapZoomEnd(e: ZoomAnimEvent) {
-    this.zoom = e.target.getZoom();
-    this.zoom$.emit(this.zoom);
+  /**
+   * Initialise a new Leaflet instance
+   */
+   initMap(): void {
+    this.newLeafletMap();
+  }
+
+  /**
+   * Add tileLayer and map based on geolocation
+   * This either generates a mapbox map or a leaflet map.
+   */
+  newLeafletMap(): void {
+    if (!this.map) {
+      this.tiles = new TileLayer(this.getTiles(), this.getTileLayerOptions());
+      this.map = new Map('map-container', MAP_OPTIONS);
+      this.map.addLayer(this.tiles);
+      this.setUserLocation();
+    }
+  }
+
+  getTiles(): string {
+    return 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+  }
+
+
+  getTileLayerOptions(): TileLayerOptions {
+
+    return {
+      crossOrigin: null,
+      // Maximum zoom number the tiles source has available.
+      // If it is specified, the tiles on all zoom levels higher than maxNativeZoom
+      // will be loaded from maxZoom level and auto-scaled.
+      maxNativeZoom: MAX_NATIVE_ZOOM
+    };
+  }
+
+  getUserLocationOptions(positionOptions?: PositionOptions): PositionOptions {
+    return {
+      enableHighAccuracy: true,
+      timeout: 20000,
+      maximumAge: 60000,
+      ...positionOptions
+    };
+  }
+
+  setUserLocation(positionOptions?: PositionOptions): void {
+    if (this.map) {
+      // success - sets the view to location, failure -  focus on London as default
+      this.map.locate({ setView: true, maxZoom: INITIAL_ZOOM, ...this.getUserLocationOptions(positionOptions) });
+
+      this.map.on('locationerror', () => {
+        this.map.setView(LONDON_COORDINATES, INITIAL_ZOOM, );
+        this.finishMapCreation();
+      });
+
+      this.map.on('locationfound', () => {
+        this.finishMapCreation();
+      });
+    }
+  }
+
+  /**
+   * Add Zoom controls and mark mark map as created
+   */
+   finishMapCreation(): void {
+    this.mapCreated$.next(true);
   }
 }
